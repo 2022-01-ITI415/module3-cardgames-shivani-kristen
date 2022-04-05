21ac7caa60e2c138cf46c25d0f9aa2b359ee6276
Shivani Patel <97494145+Shivs-p@users.noreply.github.com>
2022-04-05 18:58:53 -0400

    i am confused

--- Assets/Prospector/__Scripts/Prospector.cs
+++ Assets/Prospector/__Scripts/Prospector.cs
@@ -24,12 +24,19 @@ public class Prospector : MonoBehaviour {
 	[Header("Set in Inspector")]
 	public TextAsset deckXML;
 	public TextAsset layoutXML;
+	public float xoffset = 3;
+	public float yOffeset = -2.5f;
+	public Vector3 layoutCenter;
 
 
 	[Header("Set Dynamically")]
 	public Deck	deck;
 	public Layout layout;
 	public List<CardProspector> drawPile;
+	public Transform layoutAnchor;
+	public CardProspector target;
+	public List<CardProspector> tableau;
+	public List<CardProspector> discardPile;
 
 	void Awake(){
 		S = this;
@@ -42,6 +49,7 @@ public class Prospector : MonoBehaviour {
 		layout = GetComponent<Layout>(); // Get the Layout component
 		layout.ReadLayout(layoutXML.text); // Pass LayoutXML to it
 		drawPile = ConvertListCardsToListCardProspectors(deck.cards);
+		LayoutGame();
 	}
      List<CardProspector> ConvertListCardsToListCardProspectors(List<Card> lCD)
     {
@@ -52,5 +60,51 @@ public class Prospector : MonoBehaviour {
 			lCP.Add(tCP);
         }
 		return(lCP);
+    }
+	// this Draw function will pull a single card from the drawPile and retuen it 
+	CardProspector Draw()
+    {
+		CardProspector Draw()
+        {
+			CardProspector cd = drawPile[0]; // pull the 0th CardProspector 
+			drawPile.RemoveAt(0);
+			return (cd);
+        }
+    }
+	// LayoutGame() positions the initial tableau of cards, a.k.a the "mine"
+	void LayoutGame()
+    {
+		// Create an empty GameObject to serve as an anchor for the tableau
+		if(layoutAnchor == null)
+        {
+			GameObject tGo = new GameObject("_layoutAnchor");
+			// ^ Create an empty GameObject named _layoutAnchor in the Hierarchy
+			layoutAnchor = tGo.transform;
+			layoutAnchor.transform.position = layoutCenter;
+        }
+
+		CardProspector cp; 
+		// Follow the layout
+		foreach (SlotDef tSD in layout.slotDefs)
+        {
+			// ^ Iterate through all the SlotDefs in the layout.SlotDefs as tSD
+			cp = Draw(); // Pull a card from the top (beginning) of the draw pile 
+			cp.faceUp = tSD.faceUp;
+			cp.transform.parent = layoutAnchor; //Make its parents layoutAnchor
+												// This replaces the previous parent: deck.deckAnchor, which
+												// apperas as _Deck in the Hierarchy when the scene is playing 
+			cp.transform.localPosition = new Vector3(
+				layout.multiplier.x * tSD.x,
+				layout.multiplier.y * tSD.y,
+				-tSD.layerID);
+			// ^ Set the localPosition of the Card based on SlotDef
+			cp.layoutID - tSD.id;
+			cp.slotDef = tSD;
+			// CardProspectors in the tableau have the state CardState.tableu 
+			cp.state - eCardState.tableau;
+
+			tableau.Add(cp); // Add this CardProspector to the List<> tableau
+
+        }
     }
 }
