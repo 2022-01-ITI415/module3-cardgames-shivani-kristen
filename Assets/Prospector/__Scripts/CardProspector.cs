dcba7c4574912a690e49b0e14c5d045cd46910dc
Kristen <kristen.marcinek@rutgers.edu>
2022-04-07 20:16:46 -0400

    add scripts

---
+++ Assets/Prospector/__Scripts/CardProspector.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum eCardState (
+    drawpile,
+    tableau,
+    target,
+    discard
+)
+
+public class CardProspector : Card
+{
+    [Header("Set Dynamically: CardProspector")]
+    // this is how to use the enum eCardState
+    public eCardState state = eCardState.drawpile;
+    public List<CardProspector> hiddenBy = new List<CardProspector>();
+    public int layoutID;
+    public SlotDef slotDef;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
