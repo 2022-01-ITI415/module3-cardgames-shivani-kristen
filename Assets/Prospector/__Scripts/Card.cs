d446a8d7a88e07d07c9a28181a9e6ea515525963
Kristen <kristen.marcinek@rutgers.edu>
2022-04-28 16:07:11 -0400

    set up sorting layers

--- Assets/Prospector/__Scripts/Card.cs
+++ Assets/Prospector/__Scripts/Card.cs
@@ -15,6 +15,59 @@ public class Card : MonoBehaviour {
 	public GameObject back;  // back of card;
 	public CardDefinition def;  // from DeckXML.xml		
 
+	public SpriteRenderer[] spriteRenderers; // list of the SpriteRenderer components of this GameObject and its children
+
+	void Start() {
+		SetSortOrder(0);
+	}
+
+	// if spriteRenderers is not yet defined, this function defines it
+	public void PopulateSpriteRenderers() {
+		// if spriteRenderers is null or empty
+		if (spriteRenderers == null || spriteRenderers.Length == 0) {
+			// get SpriteRenderer components of thsi GameObject and its children
+			spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+		}
+	}
+
+	// sets the sortingLayerName on all SpriteRenderer components
+	public void SetSortingLayerName(string tSLN) {
+		PopulateSpriteRenderers();
+		
+		foreach(SpriteRenderer tSR in spriteRenderers) {
+			tSR.sortingLayerName = tSLN;
+		}
+	}
+
+	// sets the sortingOrder of all SpriteRenderer components
+	public void SetSortOrder(int sOrd) {
+		PopulateSpriteRenderers();
+
+		// iterate through all the spriteRenderers as tSR
+		foreach(SpriteRenderer tSR in spriteRenderers) {
+			if(tSR.gameObject == this.gameObject) {
+				// if this gameObject is this.gameObject, it's the background
+				tSR.sortingOrder = sOrd; //  set its order to sOrd
+				continue;
+			}
+
+			// each of the children of this GameObject are named
+			// switch based on the names
+			switch(tSR.gameObject.name) {
+				case "back": // if the name is "back
+					// set it to the highest layer to cover the other sprites
+					tSR.sortingOrder = sOrd+2;
+					break;
+				
+				case "face": // if the name is "face"
+				default: // or if it's anything else
+					// set it to the middle layer to be above the background
+					tSR.sortingOrder = sOrd+1;
+					break;
+
+			}
+		}
+	}
 
 	public bool faceUp {
 		get {
@@ -24,12 +77,6 @@ public class Card : MonoBehaviour {
 		set {
 			back.SetActive(!value);
 		}
-	}
-
-
-	// Use this for initialization
-	void Start () {
-	
 	}
 	
 	// Update is called once per frame
